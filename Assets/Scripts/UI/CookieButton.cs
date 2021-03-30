9544c7947d903abd1d09f7be71fb79f326862bd4
CSharp <Zombie-sash@yandex.ru>
2021-03-30 11:51:38 +0700

    Edit win panel

---
+++ Assets/Scripts/UI/CookieButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CookieButton : MonoBehaviour
+{
+    [SerializeField] private Sprite mainSpriteCookie;
+    [SerializeField] private Sprite[] spritesCookie;
+    [SerializeField] private Image imageCookie;
+    [SerializeField] private float timeChangeSpriteCookie;
+
+    private bool alreadyClick;
+
+    private void OnEnable()
+    {
+        imageCookie.sprite = mainSpriteCookie;
+    }
+
+    public void ClickButton()
+    {
+        alreadyClick = true;
+        StartCoroutine(CoClickButton());
+    }
+
+    private IEnumerator CoClickButton()
+    {
+        for (int i = 0; i < spritesCookie.Length; i++)
+        {
+            imageCookie.sprite = spritesCookie[i];
+            yield return new WaitForSeconds(timeChangeSpriteCookie);
+        }
+
+        UiController.Instance.ClickWinLevel();
+        alreadyClick = false;
+    }
+}
